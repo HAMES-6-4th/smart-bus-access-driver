67731638b22422ec0f04eaa50355931b1a2cc6d9
an0520a <an0520a@gmail.com>
2026-04-02 15:27:51 +0900

    can_v2_update

--- driver_with_door_can_protocol/driver_ecu_can.c
+++ driver_with_door_can_protocol/driver_ecu_can.c
@@ -2,7 +2,8 @@
 
 #include <string.h>
 
-static void DriverEcuCanBuildOpenParams(CANSocketOpenParams *params, const DriverECUCANConfig *config)
+static void DriverEcuCanBuildOpenParams(CANSocketOpenParams *params,
+                                        const DriverECUCANConfig *config)
 {
     CANSocketInitOpenParamsClassic500k(params);
 
@@ -42,13 +43,14 @@ void driverECUCANInitConfig(DriverECUCANConfig *config)
 CANStatus driverECUCANOpen(DriverECUCAN *ecu, const DriverECUCANConfig *config)
 {
     CANSocketOpenParams open_params;
+    CANStatus status;
 
     if ((ecu == 0) || (config == 0) || (config->port_name == 0))
     {
         return CAN_STATUS_EINVAL;
     }
 
-    if (CANSocketGetCoreConst(&ecu->socket) != 0)
+    if (ecu->is_open)
     {
         return CAN_STATUS_EBUSY;
     }
@@ -56,22 +58,37 @@ CANStatus driverECUCANOpen(DriverECUCAN *ecu, const DriverECUCANConfig *config)
     memset(&ecu->status, 0, sizeof(ecu->status));
     DriverEcuCanBuildOpenParams(&open_params, config);
 
-    return CANSocketOpen(&ecu->socket, &open_params);
+    status = CANSocketOpen(&ecu->socket, &open_params);
+    if (status == CAN_STATUS_OK)
+    {
+        ecu->is_open = true;
+    }
+
+    return status;
 }
 
 CANStatus driverECUCANClose(DriverECUCAN *ecu)
 {
+    CANStatus status;
+
     if (ecu == 0)
     {
         return CAN_STATUS_EINVAL;
     }
 
-    return CANSocketClose(&ecu->socket);
+    status = CANSocketClose(&ecu->socket);
+    if (status == CAN_STATUS_OK)
+    {
+        ecu->is_open = false;
+    }
+
+    return status;
 }
 
 CANStatus driverECUCANSendCommand(DriverECUCAN *ecu,
                                   uint8_t door_cmd,
                                   uint8_t ramp_cmd,
+                                  bool ramp_manual,
                                   bool emergency_stop,
                                   bool reset_fault)
 {
@@ -87,7 +104,11 @@ CANStatus driverECUCANSendCommand(DriverECUCAN *ecu,
         return CAN_STATUS_EINVAL;
     }
 
-    payload = DriverWithDoorCanPackCommand(door_cmd, ramp_cmd, emergency_stop, reset_fault);
+    payload = DriverWithDoorCanPackCommand(door_cmd,
+                                           ramp_cmd,
+                                           ramp_manual,
+                                           emergency_stop,
+                                           reset_fault);
 
     return CANSocketSendClassicStd(&ecu->socket, DOOR_CAN_ID_COMMAND, &payload, 1U);
 }
